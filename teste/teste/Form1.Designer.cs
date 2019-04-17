e85b52ff2cf1afe7c8c9ae7ded21644abb4d2020
walefismael <48078620+walefismael@users.noreply.github.com>
2019-04-17 15:21:10 -0300

    Add files via upload

---
+++ teste/teste/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbSim = new System.Windows.Forms.RadioButton();
+            this.rbNao = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.rbNao);
+            this.panel1.Controls.Add(this.rbSim);
+            this.panel1.Location = new System.Drawing.Point(183, 234);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(98, 81);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // rbSim
+            // 
+            this.rbSim.AutoSize = true;
+            this.rbSim.Location = new System.Drawing.Point(24, 29);
+            this.rbSim.Name = "rbSim";
+            this.rbSim.Size = new System.Drawing.Size(40, 17);
+            this.rbSim.TabIndex = 0;
+            this.rbSim.Text = "sim";
+            this.rbSim.UseVisualStyleBackColor = true;
+            this.rbSim.CheckedChanged += new System.EventHandler(this.RbSim_CheckedChanged);
+            // 
+            // rbNao
+            // 
+            this.rbNao.AutoSize = true;
+            this.rbNao.Location = new System.Drawing.Point(24, 53);
+            this.rbNao.Name = "rbNao";
+            this.rbNao.Size = new System.Drawing.Size(43, 17);
+            this.rbNao.TabIndex = 1;
+            this.rbNao.Text = "não";
+            this.rbNao.UseVisualStyleBackColor = true;
+            this.rbNao.CheckedChanged += new System.EventHandler(this.RbNao_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(183, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(181, 138);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(183, 81);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 5;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(94, 405);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(351, 20);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "cor da pele";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "vc e gay?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rbNao;
+        private System.Windows.Forms.RadioButton rbSim;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ teste/teste/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste
+{
+    public partial class Form1 : Form
+    {
+        
+            
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("boa");
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (txt1.Text == string.Empty || txt2.Text == string.Empty || (rbSim.Checked == false && rbNao.Checked == false))
+            {
+                MessageBox.Show("Preencha tds campos");
+            }
+
+            else
+            {
+
+                if (rbSim.Checked == true)
+                {
+                    txtResultado.Text = "foi marcado";
+
+                }
+                else
+                {
+                    rbNao.Checked = false;
+
+                    txtResultado.Text = "nao foi marcado";
+                }
+
+                rbSim.Checked = false;
+                rbNao.Checked = false;
+            }
+        }
+            private void RbNao_CheckedChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void RbSim_CheckedChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void Panel1_Paint(object sender, PaintEventArgs e)
+            {
+
+            }
+        
+    }
+}
